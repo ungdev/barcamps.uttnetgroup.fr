0b3b024b30c9fd292a3e90bfa17e843a733e7713
prudywsh <antoineprudhomme5@gmail.com>
2018-03-07 13:56:06 +0100

    read port number in env. By default, the port number is 8080

--- serve.js
+++ serve.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 
+const port = process.env.PORT || '8080';
+
 app.use(express.static('build'));
 app.get('*', (req, res) => {
   res.sendfile('./build/index.html');
 })
 
-app.listen('8080', _ => console.log('Server is listening on port 8080'))
+app.listen(port, _ => console.log(`Server is listening on port ${port}`))
