be00978bcdc3f355f5877c4514b3681fa4108662
Florian Esslinger <florian.esslinger@live.fr>
2018-04-06 22:38:25 +0200

    Add listTalk in popover

--- src/components/SpeakerInfo.jsx
+++ src/components/SpeakerInfo.jsx
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 
+import SpeakerService from '../services/SpeakerService'
 import '../styles/Talk.css';
 
 
 
 class SpeakerInfo extends Component {
 
+  constructor(){
+    super();
+    this.state = {
+      email: '',
+      listTalks: [<li></li>,<li></li>]
+    }
+  }
+
+  componentDidMount() {
+    SpeakerService.getID(this.props.speaker.id)
+    .then(speaker => {
+      this.setState({email : speaker.email});
+      let listTalks = speaker.talks.map(talk => <li>{talk.title}</li>);
+      this.setState({listTalks})
+    });
+  }
+
   render() {
 
     return(
       <div>
-        {this.props.speaker.email}
+        Email: {this.state.email} <br/>
+        Liste des présentations:
+        <ul>{this.state.listTalks}</ul>
       </div>
     )
   }
