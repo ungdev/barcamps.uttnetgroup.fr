2f9be0bad5cff98f2bc46456591177011421403c
florianess <florian.esslinger@live.fr>
2018-04-16 22:20:34 +0200

    Fonctionnal delete button

--- src/components/DisplayBarcamps.jsx
+++ src/components/DisplayBarcamps.jsx
@@ -1,31 +1,40 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Glyphicon, Button, ButtonGroup } from 'react-bootstrap'
+import { Glyphicon, Button, ButtonGroup,Modal } from 'react-bootstrap'
 
 import CreateForm from './CreateForm.jsx';
 import DisplayTalks from './DisplayTalks.jsx'
-//import { fetchTalks } from '../actions/index'
+import { fetchTalks, deleteBarcamp } from '../actions/index'
+import BarcampService from '../services/BarcampService'
 import '../styles/App.css'
 class DisplayBarcamps extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    };
+    this.delete = this.delete.bind(this);
+  }
 
   showBarcamps(barcamps) {
       return barcamps.map((barcamp) =>{
         const date = new Date(barcamp.date)
-        return (<div><h2 className='Barcamp'>{barcamp.title} le {date.toLocaleDateString()} {this.edit()}</h2>
+        return (<div><h2 className='Barcamp'>{barcamp.title} le {date.toLocaleDateString()} {this.edit(barcamp)}</h2>
         <hr width="78%" color="grey"/>
         <DisplayTalks talks = {barcamp.talks}/></div>)
       });
   }
 
-  edit() {
-    if (this.props.admin.user.admin) {//true) {
+  edit(barcamp) {
+    if (this.props.admin.user.admin) {//true
       return (
         <div className="editButton">
         <ButtonGroup>
           <Button bsSize="small">
             <Glyphicon glyph="pencil"/>
           </Button>
-          <Button bsSize="small">
+          <Button bsSize="small" onClick={() => this.handleShow(barcamp)}>
             <Glyphicon glyph="trash"/>
           </Button>
         </ButtonGroup>
@@ -36,6 +45,17 @@ class DisplayBarcamps extends Component {
     }
   }
 
+  handleShow(barcamp) {
+    this.setState({show : true, barcamp})
+  }
+
+  delete() {
+    this.setState({show : false});
+    BarcampService.deleteID(this.props.admin.token,this.state.barcamp.id);
+    this.props.dispatch(deleteBarcamp(this.state.barcamp.id));
+    this.props.dispatch(fetchTalks());
+  }
+
   render() {
     let barcamps = this.props.barcamps ? this.showBarcamps(this.props.barcamps)
     : [];
@@ -44,6 +64,14 @@ class DisplayBarcamps extends Component {
         <div>
           <CreateForm />
           {barcamps}
+          <Modal show={this.state.show} onHide={() => this.setState({show: false})}>
+            <Modal.Header closeButton>
+              Confirmer la suppression du barcamp
+            </Modal.Header>
+            <Modal.Body>
+              <Button bsStyle="danger" onClick={this.delete}>Supprimer</Button>
+            </Modal.Body>
+          </Modal>
         </div>
     );
   }
